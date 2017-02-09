11df38225dedf4fcf0637c189d816a09ef8e4255
curtisjohnson1 <curtisianjohnson@gmail.com>
2017-02-09 17:51:01 +0000

    create 4 of 16 functions - last & each update

--- lowbar.js
+++ lowbar.js
@@ -13,6 +13,39 @@ _.first = function(array, n) {
     result.push(array[i]); i++;
   };
   return result;
+};
+
+_.last = function(array, n)  {
+  if(arguments.length === 1) {
+    var reverse = array.reverse();
+    return reverse[0];
+};
+  // loop through array from n
+  // push values into new array
+  var result = [];
+  for(var i = n-1; i < array.length; i++) {
+    result.push(array[i])
+  }
+  return result;
+};
+
+_.each = function(list, iteratee)  {
+  // IF array
+  // loop through array
+  // for each iteratee - console.log
+  // else 
+  //  array.forEach(function(element)  {
+    //  iteratee(element)});
+    if(Array.isArray(list)) {
+    for (var i = 0; i < list.length; i++) {
+      iteratee(list[i]);
+    }
+  }
+  else {
+    for(var key in list) {
+      iteratee(list[key])
+    }
+  }
 }
 
 if (typeof module !== 'undefined') {
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var expect = require('chai').expect;
+var sinon = require('sinon')
 
 var _ = require(path.join(__dirname, '..', './lowbar.js'));
 
@@ -28,4 +29,30 @@ describe('_', function () {
       expect(_.first([1, 2, 3], 2)).to.eql([1, 2]);
     });
   });
+  describe('#last', function ()  {
+    it('is a function', function ()  {
+      expect(_.last).to.be.a('function');
+    });
+    it('returns the last element of an array', function ()  {
+      expect(_.last([1,2,3])).to.equal(3);
+    });
+    it('returns the last n elements of an array', function ()  {
+      expect(_.last([1,2,3],2)).to.eql([2,3]);
+    })
+  });
+  describe('#each', function ()  {
+    it('is a function', function ()  {
+      expect(_.each).to.be.a('function');
+    });
+    it('passes each element of a list into a function', function ()  {
+      var spy = sinon.spy();
+      var output = _.each([1,2,3,4,5], spy);
+      expect(spy.callCount).to.eql(5)
+    });
+    it('passes each element of an object into a function', function ()  {
+      var spy = sinon.spy();
+      var output = _.each({a:1, b:2, c:3}, spy);
+      expect(spy.callCount).to.eql(3)
+    })
+  })
 });
